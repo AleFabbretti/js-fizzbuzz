98f6e6f8d88aec64cf83b1d7b3762987f765b1a1
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-25 11:04:03 +0200

    Aggiunto correzione esercizio

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ Scrivi un programma che stampi in console i numeri da 1 a 100,
 ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. 
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
-const table = document.getElementById("table");
+const tableContainer = document.getElementById("table");
 
 for(let i = 1; i <= 100; i++){
     //creo un div e gli assegno una classe
@@ -14,27 +14,25 @@ for(let i = 1; i <= 100; i++){
     //se il numero che stampo è multiplo di 3 e 5 stampo FizzBuzz
             div.classList.add("col-fizz-buzz");
             // console.log("FizzBuzz");
-            div.innerHTML = "FizzBuzz"
-            table.append(div);
+            div.innerHTML = "FizzBuzz";
         }else if(i % 3 == 0){
     //se il numero che stampo è multiplo di 3 stampo Fizz
             div.classList.add("col-fizz");
             // console.log("Fizz");
             div.innerHTML = "Fizz";
-            table.append(div);
         }else if(i % 5 == 0){
     //se il numero che stampo è multiplo di 5 stampo Buzz
             div.classList.add("col-buzz");
             // console.log("Buzz");
             div.innerHTML = "Buzz";
-            table.append(div);
         }else{
     //se nessuna delle condizioni sopra stampo il numero corrispondente ad i
             console.log(i);
             div.classList.add("col-number");
             div.innerHTML = i;
-            table.append(div);
         }
+    //inserire l'item nel suo contenitore (tableContainer)    
+        table.append(div);
     }
 
 
